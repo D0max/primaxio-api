103ca97da01c5223cc7955b6eac37cd00cc93bb1
Sobolev Maksim <sobolezm@gmail.com>
2019-09-20 04:03:07 +0600

    update

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
-const app = express();
 
+const app = express();
+app.get('/', (req, res) => {
+  res.send('ok')
+})
 app.use(express.json());
 
-const port = process.env.BASE_URL_BACK;
+const port = process.env.BASE_URL_BACK || 8000;
+
+
 
 app.listen(port, () => {
   console.log(`Server started on ${port} port `)
